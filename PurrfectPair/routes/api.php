1db147f004f253a5c149d5a168a630001cbde98e
Shierwin <shierwinsandino@gmail.com>
2025-03-14 00:46:48 +0800

    modified api.php file

--- PurrfectPair/routes/api.php
+++ PurrfectPair/routes/api.php
@@ -3,37 +3,38 @@ require_once __DIR__ . '/../controllers/AuthController.php';
 require_once __DIR__ . '/../controllers/CatController.php';
 require_once __DIR__ . '/../config/Database.php';
 
+$router = new AltoRouter();
 $db = new Database();
 $authController = new AuthController($db);
 $catController = new CatController($db);
 
 // Auth routes
-$router->post('/register', function() use ($authController) {
+$router->map('POST', '/register', function() use ($authController) {
     $authController->register();
 });
 
-$router->post('/login', function() use ($authController) {
+$router->map('POST', '/login', function() use ($authController) {
     $authController->login();
 });
 
-$router->post('/change-password', function() use ($authController) {
+$router->map('POST', '/change-password', function() use ($authController) {
     $authController->changePassword();
 });
 
 // Cat routes
-$router->get('/cats', function() use ($catController) {
+$router->map('GET', '/cats', function() use ($catController) {
     $catController->getAllCats();
 });
 
-$router->post('/cats', function() use ($catController) {
+$router->map('POST', '/cats', function() use ($catController) {
     $catController->addCat();
 });
 
-$router->put('/cats/{id}', function($id) use ($catController) {
+$router->map('PUT', '/cats/{id}', function($id) use ($catController) {
     $catController->updateCat($id);
 });
 
-$router->delete('/cats/{id}', function($id) use ($catController) {
+$router->map('DELETE', '/cats/{id}', function($id) use ($catController) {
     $catController->deleteCat($id);
 });
 ?>
